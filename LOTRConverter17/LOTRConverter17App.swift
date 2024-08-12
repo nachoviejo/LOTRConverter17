459d0f7080813277e15abc0fe792880dc337c9eb
iviejo_genasys <iviejo@genasys.com>
2024-08-12 09:46:56 -0300

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Kinamic Kinamic on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
